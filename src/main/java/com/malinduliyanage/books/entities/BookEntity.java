4688637aed35c9c1c8384203f7ee267b2bc46a67
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-05-20 11:47:48 +0530

    Database Configuration with Dev Env

---
+++ src/main/java/com/malinduliyanage/books/entities/BookEntity.java
@@ -0,0 +1,47 @@
+//Entity is a Java class that maps to a table in the database
+//It Represents the data structure
+//Used for defining columns, primary key, timestamps, etc. and maintains data
+
+package com.malinduliyanage.books.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+//We use JPA (Java Persistent API) for
+@Setter
+@Getter
+// @Entity marks this class as a JPA entity (i.e., it maps to a DB table)
+@Entity
+@Table(name = "story")
+public class BookEntity {
+
+    //@Id → Marks this field as the primary key
+    //@GeneratedValue(...) → Lets the DB auto-generate the value (e.g., AUTO_INCREMENT in MySQL)
+    @Id
+    // strategy specifies how the ID should be generated
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    //Can state @Column to pointout which column we consider in the selected table
+    //If not, it takes the variable name as corresponding to the col name
+    @Column(name = "book_name")
+    private String bookName;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    private String description;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
---
+++ src/main/java/com/malinduliyanage/books/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+//Repository is a Java interface used to perform DB operations on entities
+//Handles CRUD logic
+
+package com.malinduliyanage.books.repositories;
+
+import com.malinduliyanage.books.entities.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<BookEntity, Integer> {
+
+}
