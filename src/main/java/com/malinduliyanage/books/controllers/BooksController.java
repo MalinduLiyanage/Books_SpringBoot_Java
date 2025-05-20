12495ecec1650bb35798249918691fcc33ed7661
Malindu Liyanage <136006504+MalinduLiyanage@users.noreply.github.com>
2025-05-20 10:11:46 +0530

    Basic Controller Configurations

---
+++ src/main/java/com/malinduliyanage/books/controllers/BooksController.java
@@ -0,0 +1,24 @@
+package com.malinduliyanage.books.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//We use @RestController to create RESTful web services.
+//Automatically adds @ResponseBody to all methods, meaning the returned data (like JSON) is sent directly in the HTTP response body, not as a view name.
+@RestController
+//RequestMapping maps a specific URL path to a class or method, helps to group related endpoints under one base path.
+@RequestMapping("/api/books")
+public class BooksController {
+
+    //We can map to a specific path, or leave it empty
+    @PostMapping("/test")
+    public String test(@RequestBody String name) {
+
+        //@RequestBody binds the HTTP request body to a method parameter
+        //Without it, Spring will look for a query param or form param like api/books?name=J.K. Rowling
+
+        return "Book " + name + " added!";
+    }
+}
--- src/main/java/com/malinduliyanage/books/controllers/HomeController.java
+++ src/main/java/com/malinduliyanage/books/controllers/HomeController.java
@@ -4,17 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//We use @Controller to return a View
 @Controller
+//RequestMapping maps a specific URL path to a class or method, helps to group related endpoints under one base path.
+@RequestMapping("/")
 public class HomeController {
 
+    //Reads metadata from application.yaml
     @Value("${spring.application.name}")
-    private String applicationName;
+    private String successMsg;
 
-    @RequestMapping("/")
     public String index () {
 
-        System.out.println(applicationName);
-
+        System.out.println(successMsg);
         return "index.html";
     }
 }
